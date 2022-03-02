17501e214b7098bbdf523d2c2a1d1ab253025b06
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-03-02 14:46:48 +0100

    added constant variable to finction

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 21:21:47 by ivork         #+#    #+#                 */
-/*   Updated: 2022/02/16 16:31:47 by ivork         ########   odam.nl         */
+/*   Updated: 2022/03/02 14:22:31 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 #include <math.h>
 #include <fractol.h>
 
-unsigned int	julia_set(t_complex complex)
+unsigned int	julia_set(t_complex complex, t_complex constant)
 {
 	t_complex	z;
-	t_complex	constant;
 	double		n;
 
 	n = 0.0;
 	z.imaginary = complex.imaginary;
 	z.real = complex.real;
-	constant.real = complex.constant->real;
-	constant.imaginary = complex.constant->imaginary;
 	while (absolute_complex(z) < 4 && (n < MAX_ITER))
 	{
 		z = multiply_complex_num(z, z);
@@ -34,11 +31,12 @@ unsigned int	julia_set(t_complex complex)
 	return (n);
 }
 
-unsigned int	mandelbrot_set(t_complex complex)
+unsigned int	mandelbrot_set(t_complex complex, t_complex constant)
 {
 	t_complex	z;
 	double		n;
 
+	(void)constant;
 	n = 0.0;
 	z.imaginary = 0;
 	z.real = 0;
