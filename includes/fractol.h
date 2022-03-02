02e44f6a49a55016bde9e8c31022368428472449
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-03-02 14:45:34 +0100

    chagnged prototype

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 21:27:48 by ivork         #+#    #+#                 */
-/*   Updated: 2022/02/16 15:52:33 by ivork         ########   odam.nl         */
+/*   Updated: 2022/03/02 14:21:33 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 # define MAX_ITER 100
 # include <complex_num.h>
 
-unsigned int	julia_set(t_complex complex);
-unsigned int	mandelbrot_set(t_complex complex);
+unsigned int	julia_set(t_complex complex, t_complex constant);
+unsigned int	mandelbrot_set(t_complex complex, t_complex constant);
 #endif
